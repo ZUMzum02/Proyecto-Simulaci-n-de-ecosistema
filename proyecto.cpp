7e7d919107a4f4ec309c7f822c10d9571deac701
ZUMzum02 <paololovon@icloud.com>
2025-11-14 02:03:03 -0500

    Implement movement for organisms and random seed
    Added random movement functionality and updated organism classes to include movement methods.

--- proyecto.cpp
+++ proyecto.cpp
@@ -5,6 +5,13 @@ const int FILAS = 5;
 const int COLUMNAS = 5;
 const int MAX_ORGANISMOS = 10;
 
+// funcion de aleatoriedad 
+int semilla = 123; 
+int aleatorio(int maximo) {
+    semilla = (semilla * 1103515245 + 12345) % 2147483648;
+    return semilla % maximo;
+}
+
 
 class Organismo {
 public:
@@ -13,7 +20,7 @@ public:
     Organismo(int px = 0, int py = 0, int pvida = 5) : x(px), y(py), vida(pvida) {}
 
     virtual char simbolo() const = 0;
-    // virtual void mover() {}
+    virtual void mover(int filas, int columnas) = 0;
     // virtual void comer() {}
     virtual void vivir() {
          vida--; 
@@ -27,18 +34,30 @@ class Vegetacion : public Organismo {
 public:
     Vegetacion(int px, int py) : Organismo(px, py, 4) {}
     char simbolo() const { return 'V'; }
+    void mover(int filas, int columnas) {}
+
 };
 
 class Presa : public Organismo {
 public:
     Presa(int px, int py) : Organismo(px, py, 6) {}
     char simbolo() const { return 'P'; }
+    void mover(int filas, int columnas) {
+    y++;
+    if (y >= columnas) y = 0;
+}
+
 };
 
 class Depredador : public Organismo {
 public:
     Depredador(int px, int py) : Organismo(px, py, 8) {}
     char simbolo() const { return 'D'; }
+    void mover(int filas, int columnas) {
+    x++;
+    if (x >= filas) x = 0;
+}
+
 };
 
 class Mundo {
@@ -82,6 +101,14 @@ public:
             cout << endl;
         }
     }
+    void avanzarDia() {
+    for (int i = 0; i < cantidad; i++) {
+        if (organismos[i]->estaVivo()) {
+            organismos[i]->mover(FILAS, COLUMNAS);
+            organismos[i]->vivir();
+        }
+    }
+}
 };
 
 
@@ -103,3 +130,4 @@ int main() {
 
     return 0;
 }
+
