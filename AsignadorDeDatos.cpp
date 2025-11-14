0dfb9e243fabcd70c1de7eaf32a62156b4438d23
ALUMNO-D104 <ALUMNO-D104@UCSP.AULAB>
2025-11-14 08:27:23 -0500

    Asignador de Datos Organismo

---
+++ AsignadorDeDatos.cpp
@@ -0,0 +1,10 @@
+#include "AsignadorDeDatos.h"
+
+static void AsignadorDeDatos::completarData(Organismo &org)
+{
+    if(org.nombre == "Leon")
+    {
+        org.especie = "Mamifero";
+    }
+    else if(org.nombre == " ")
+}
---
+++ AsignadorDeDatos.h
@@ -0,0 +1,11 @@
+#ifndef ASIGNADORDEDATOS_H
+#define ASIGNADORDEDATOS_H
+#include "Organismo.h"
+
+class AsignadorDeDatos
+{
+    public:
+        static void completarData(Organismo &org);
+};
+
+#endif // ASIGNADORDEDATOS_H
